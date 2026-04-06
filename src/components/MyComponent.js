b6e0bd8aaf7f38b3097b4e808aec5311f1ae3836
ThinhChu <chuquangthinh34@gmail.com>
2026-04-06 11:36:02 +0700

    From in react

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -21,6 +21,18 @@ class Mycomponent extends React.Component {
     console.log("Đã hover thành công");
   }
 
+  handleChange = (event) => {
+    // console.log(event.target.value);
+    this.setState({
+      name: event.target.value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+  };
+
   //JSX
   render() {
     return (
@@ -28,6 +40,10 @@ class Mycomponent extends React.Component {
         My name is {this.state.name} and i'm from {this.state.address}
         <button onClick={this.handleClick}>Click</button>
         <button onMouseOver={this.handleHover}>Hover</button>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" onChange={this.handleChange} />
+          <button type="submit">Gửi</button>
+        </form>
       </div>
     );
   }
