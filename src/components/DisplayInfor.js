8f1801185289ba506d192de71fe2b5c173d2066f
ThinhChu <chuquangthinh34@gmail.com>
2026-04-06 16:24:09 +0700

    Outputting List

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -4,11 +4,19 @@ class DisplayInfor extends React.Component {
   render() {
     // console.log(this.props);
     // Destructuring array/object
-    const { name, age } = this.props;
+    const { listUsers } = this.props;
+    // console.log(listUsers);
     return (
       <div>
-        <div>My name is {name}</div>
-        <div>I'm {age}</div>
+        {listUsers.map((user) => {
+          return (
+            <div key={user.id}>
+              <div>My name is {user.name}</div>
+              <div>I'm {user.age}</div>
+              <hr />
+            </div>
+          );
+        })}
       </div>
     );
   }
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -6,14 +6,22 @@ import UseInfo from "./UserInfo";
 import DisplayInfor from "./DisplayInfor";
 
 class Mycomponent extends React.Component {
+  state = {
+    listUsers: [
+      { id: 1, name: "Thịnh", age: 25 },
+      { id: 2, name: "Hsasd", age: 30 },
+      { id: 3, name: "Lắqqe", age: 56 },
+    ],
+  };
   //JSX
   render() {
     const myInfor = ["a", "b", "c"];
+    // DRY: Don't Repeat yourseft
     return (
       <div>
         <UseInfo />
         <br />
-        <DisplayInfor name={"Th"} age={15} myInfor={myInfor} />
+        <DisplayInfor listUsers={this.state.listUsers} />
       </div>
     );
   }
