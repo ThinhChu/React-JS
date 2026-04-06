cfb02a9b4280fd474f6181c7a9471c0ee7dad12b
ThinhChu <chuquangthinh34@gmail.com>
2026-04-06 10:19:04 +0700

    React Component

---
+++ src/components/MyComponent.js
@@ -0,0 +1,12 @@
+// class component
+// function component
+
+import React from "react";
+
+class Mycomponent extends React.Component {
+  render() {
+    return <div>my first component</div>;
+  }
+}
+
+export default Mycomponent;
