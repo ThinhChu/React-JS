93f8a0c66904d64d7485b874efd9a25eddc9fe8c
ThinhChu <chuquangthinh34@gmail.com>
2026-04-06 15:15:40 +0700

    Nesting Component

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,50 +2,12 @@
 // function component
 
 import React from "react";
+import UseInfo from "./UserInfo";
 
 class Mycomponent extends React.Component {
-  state = {
-    name: "Thịnh",
-    address: "511/20 Thống Nhất",
-    age: 25,
-  };
-
-  handleClick = (event) => {
-    this.setState({
-      name: "Sdadas",
-    });
-    console.log("Đã click thành công", this.state.name);
-  };
-
-  handleHover(event) {
-    console.log("Đã hover thành công");
-  }
-
-  handleChange = (event) => {
-    // console.log(event.target.value);
-    this.setState({
-      name: event.target.value,
-    });
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(this.state);
-  };
-
   //JSX
   render() {
-    return (
-      <div>
-        My name is {this.state.name} and i'm from {this.state.address}
-        <button onClick={this.handleClick}>Click</button>
-        <button onMouseOver={this.handleHover}>Hover</button>
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" onChange={this.handleChange} />
-          <button type="submit">Gửi</button>
-        </form>
-      </div>
-    );
+    return <UseInfo></UseInfo>;
   }
 }
 
---
+++ src/components/UserInfo.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+class UseInfo extends React.Component {
+  state = {
+    name: "Thịnh",
+    address: "511/20 Thống Nhất",
+    age: 25,
+  };
+
+  handleChangeName = (event) => {
+    // console.log(event.target.value);
+    this.setState({
+      name: event.target.value,
+    });
+  };
+
+  handleChangeAge = (event) => {
+    // console.log(event.target.value);
+    this.setState({
+      age: event.target.value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+  };
+  render() {
+    return (
+      <div>
+        My name is {this.state.name} and i'm from {this.state.address}, i'm{" "}
+        {this.state.age}
+        <button onClick={this.handleClick}>Click</button>
+        <button onMouseOver={this.handleHover}>Hover</button>
+        <form onSubmit={this.handleSubmit}>
+          <label>Tên:</label>
+          <input type="text" onChange={this.handleChangeName} />
+          <label>Tuổi</label>
+          <input type="text" onChange={this.handleChangeAge} />
+          <button type="submit">Gửi</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default UseInfo;
