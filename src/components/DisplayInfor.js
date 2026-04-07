309d1fe08e77bf0b00a34bea8c785dc3e46b2b5f
ThinhChu <chuquangthinh34@gmail.com>
2026-04-07 11:20:14 +0700

    stateful và stateless

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -2,72 +2,69 @@ import React from "react";
 import "./style/DisplayInfor.scss";
 // import logo from "../logo.svg";
 
-class DisplayInfor extends React.Component {
-  constructor(props) {
-    console.log("check constructor");
+// class DisplayInfor extends React.Component {
 
-    super(props);
-    //Babel compiler
-    this.state = {
-      isShowListUsers: true,
-    };
-  }
+//   render() {
+//     // Destructuring array/object
+//     const { listUsers } = this.props;
+//     // console.log(listUsers);
 
-  handleShowHide = (e) => {
-    // console.log(this.state.isShowListUsers);
-    // !this.state.isShowListUsers lấy giá trị ngược lại với giá trị ban đầu
-    this.setState({
-      isShowListUsers: !this.state.isShowListUsers,
-    });
-  };
+//     return (
+//       <div className="displayInforItem">
 
-  componentDidMount() {
-    console.log("check componentDidMount");
-  }
+//         {true && (
+//           <div>
+//             {listUsers.map((user) => {
+//               return (
+//                 // +user.age (+) đứng trước biến đổi kiểu (type) giá trị thành number
+//                 <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+//                   <div>My name is {user.name}</div>
+//                   <div>I'm {user.age}</div>
+//                   <button
+//                     onClick={() => {
+//                       this.props.handleDeleteUser(user.id);
+//                     }}
+//                   >
+//                     Delete
+//                   </button>
+//                   <hr />
+//                 </div>
+//               );
+//             })}
+//           </div>
+//         )}
+//       </div>
+//     );
+//   }
+// }
 
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    console.log("check componentDidUpdate");
-  }
-
-  render() {
-    console.log("check render");
-    // console.log(this.props);
-    // Destructuring array/object
-    const { listUsers } = this.props;
-    // console.log(listUsers);
-
-    return (
-      <div className="displayInforItem">
-        {/* <img src={logo} alt="sfsdfsd" /> */}
+const DisplayInfor = (props) => {
+  const { listUsers } = props;
+  return (
+    <div className="displayInforItem">
+      {true && (
         <div>
-          <button onClick={this.handleShowHide}>
-            {this.state.isShowListUsers ? "Hide" : "Show"}
-          </button>
+          {listUsers.map((user) => {
+            return (
+              // +user.age (+) đứng trước biến đổi kiểu (type) giá trị thành number
+              <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+                <div>My name is {user.name}</div>
+                <div>I'm {user.age}</div>
+                <button
+                  onClick={() => {
+                    props.handleDeleteUser(user.id);
+                  }}
+                >
+                  Delete
+                </button>
+                <hr />
+              </div>
+            );
+          })}
         </div>
-        {this.state.isShowListUsers && (
-          <div>
-            {listUsers.map((user) => {
-              return (
-                // +user.age (+) đứng trước biến đổi kiểu (type) giá trị thành number
-                <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-                  <div>My name is {user.name}</div>
-                  <div>I'm {user.age}</div>
-                  <button
-                    onClick={() => {
-                      this.props.handleDeleteUser(user.id);
-                    }}
-                  >
-                    Delete
-                  </button>
-                  <hr />
-                </div>
-              );
-            })}
-          </div>
-        )}
-      </div>
-    );
-  }
-}
+      )}
+    </div>
+  );
+};
 
 export default DisplayInfor;
