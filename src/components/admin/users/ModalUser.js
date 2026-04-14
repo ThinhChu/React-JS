d1e27c0725fedd64a8162c8a89df349359c89880
ThinhChu <chuquangthinh34@gmail.com>
2026-04-14 11:27:35 +0700

    Valid và sử dụng react-toast tạo thông báo

--- src/components/admin/users/ModalUser.js
+++ src/components/admin/users/ModalUser.js
@@ -1,16 +1,25 @@
-// import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import "../../../assets/scss/modalUser.scss";
-import React, { useState } from "react";
+import { useState } from "react";
 import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const ModalUser = (props) => {
   const { show, setShow } = props;
-  const handleClose = () => setShow(false);
+  const handleClose = () => {
+    setShow(false);
+    setUserName("");
+    setRole("");
+    setEmail("");
+    setPassword("");
+    setSelectedImage("");
+    setPreviewUrl("");
+  };
 
   const [userName, setUserName] = useState("");
   const [role, setRole] = useState("");
@@ -50,102 +59,128 @@ const ModalUser = (props) => {
     setPassword(event.target.value);
   };
 
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      );
+  };
+
   const handleCreateUser = async (event) => {
-    // console.log();
+    // Validation
+    const checkEmail = validateEmail(email);
+    if (!checkEmail) {
+      toast.error("Vui lòng kiểm tra lại mục email");
+      return;
+    }
+    // Data
     const data = new FormData();
     data.append("username", userName);
     data.append("role", role);
     data.append("email", email);
     data.append("password", password);
     data.append("userImage", selectedImage);
-    let res = await axios.post(
-      "http://localhost:8081/api/v1/participant",
-      data,
-    );
-    console.log("check responsive >>", res);
+    try {
+      let res = await axios.post(
+        "http://localhost:8081/api/v1/participant",
+        data,
+      );
+      if (res.data.EC === 1) {
+        toast.info(res.data.EM);
+      } else {
+        toast.success(res.data.EM);
+        handleClose();
+      }
+    } catch (error) {
+      console.log("check responsive >>", error);
+    }
   };
 
   return (
-    <Modal show={show} size={props.size} onHide={() => props.setShow(false)}>
-      <Modal.Header closeButton>
-        <Modal.Title>Add User</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Form>
-          <Row className="mb-3">
-            <Form.Group as={Col} controlId="formGridUsername">
-              <Form.Label>Username</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter username"
-                autoComplete="Username"
-                defaultValue={userName}
-                onChange={handleInputUserName}
-              />
-            </Form.Group>
-            <Form.Group as={Col} controlId="formGridState">
-              <Form.Label>Role</Form.Label>
-              <Form.Select defaultValue={role} onChange={handleInputRole}>
-                <option>Choose...</option>
-                <option>Member</option>
-                <option>Admin</option>
-              </Form.Select>
-            </Form.Group>
-          </Row>
-          <Row className="mb-3">
-            <Form.Group as={Col} controlId="formGridEmail">
-              <Form.Label>Email</Form.Label>
-              <Form.Control
-                type="email"
-                defaultValue={email}
-                placeholder="Enter email"
-                onChange={handleInputEmail}
-              />
-            </Form.Group>
-
-            <Form.Group as={Col} controlId="formGridPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                defaultValue={password}
-                placeholder="Password"
-                autoComplete="current-password"
-                onChange={handleInputPassword}
-              />
-            </Form.Group>
-          </Row>
-          <Form.Group controlId="formFile" className="mb-3">
-            <Form.Label>Image thumbnail</Form.Label>
-            <div className="mb-3 text-center preview-image">
-              <Form.Control
-                type="file"
-                accept="image/*"
-                className="file-hidden"
-                onChange={handleFileImage}
-              />
-              {previewUrl ? (
-                <img
-                  src={previewUrl}
-                  className="img-thumbnail"
-                  alt="preview"
-                  style={{ maxHeight: "200px" }}
+    <>
+      <Modal show={show} size={props.size} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add User</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Row className="mb-3">
+              <Form.Group as={Col} controlId="formGridUsername">
+                <Form.Label>Username</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter username"
+                  autoComplete="Username"
+                  defaultValue={userName}
+                  onChange={handleInputUserName}
                 />
-              ) : (
-                <span>Upload image</span>
-              )}
-            </div>
-          </Form.Group>
-        </Form>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={handleClose}>
-          Close
-        </Button>
-        <Button variant="primary" onClick={() => handleCreateUser()}>
-          Save Changes
-        </Button>
-      </Modal.Footer>
-    </Modal>
+              </Form.Group>
+              <Form.Group as={Col} controlId="formGridState">
+                <Form.Label>Role</Form.Label>
+                <Form.Select defaultValue={role} onChange={handleInputRole}>
+                  <option>Choose...</option>
+                  <option>Member</option>
+                  <option>Admin</option>
+                </Form.Select>
+              </Form.Group>
+            </Row>
+            <Row className="mb-3">
+              <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  type="email"
+                  defaultValue={email}
+                  placeholder="Enter email"
+                  onChange={handleInputEmail}
+                />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  defaultValue={password}
+                  placeholder="Password"
+                  autoComplete="current-password"
+                  onChange={handleInputPassword}
+                />
+              </Form.Group>
+            </Row>
+            <Form.Group controlId="formFile" className="mb-3">
+              <Form.Label>Image thumbnail</Form.Label>
+              <div className="mb-3 text-center preview-image">
+                <Form.Control
+                  type="file"
+                  accept="image/*"
+                  className="file-hidden"
+                  onChange={handleFileImage}
+                />
+                {previewUrl ? (
+                  <img
+                    src={previewUrl}
+                    className="img-thumbnail"
+                    alt="preview"
+                    style={{ maxHeight: "200px" }}
+                  />
+                ) : (
+                  <span>Upload image</span>
+                )}
+              </div>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={() => handleCreateUser()}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <ToastContainer />
+    </>
   );
 };
 
